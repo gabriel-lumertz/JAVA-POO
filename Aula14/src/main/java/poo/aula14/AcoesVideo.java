83a09cd4bea971306dfef535c54809e96a839a3d
Gabriel Lumertz <61668558+gabriel-lumertz@users.noreply.github.com>
2023-06-06 22:59:33 -0300

    Aula Prática 14
    Nessa aula de POO, vamos iniciar a construção de um exemplo completo de um modelo 100% construído em Programação Orientada a Objetos com Java.

---
+++ Aula14/src/main/java/poo/aula14/AcoesVideo.java
@@ -0,0 +1,11 @@
+package poo.aula14;
+
+/**
+ *
+ * @author gabriel@lumertz.dev
+ */
+public interface AcoesVideo {
+    public void play();
+    public void pause();
+    public void like();
+}
---
+++ Aula14/src/main/java/poo/aula14/Aula14.java
@@ -0,0 +1,22 @@
+package poo.aula14;
+
+/**
+ *
+ * @author gabriel@lumertz.dev
+ */
+public class Aula14 {
+
+    public static void main(String[] args) {
+        Video v[] = new Video[3];
+        v[0] = new Video("Aula 1 de POO");
+        v[1] = new Video("Aula 12 de PHP");
+        v[2] = new Video("Aula 10 de HTML5");
+        
+        Gafanhoto g[] = new Gafanhoto[2];
+        g[0] = new Gafanhoto("Jubileu", 22, "M", "Juba");
+        g[1] = new Gafanhoto("Creusa", 12, "F", "Creuzita");
+        
+        System.out.println(v[0].toString());
+        System.out.println(g[0].toString());
+    }
+}
